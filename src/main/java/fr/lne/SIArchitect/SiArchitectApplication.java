49861a6c050a89d42db0b035563d65845c18b741
El Ayadi Yassir <yassir.elayadi@lne.fr>
2025-03-25 14:18:05 +0100

    'lancement'

---
+++ src/main/java/fr/lne/SIArchitect/SiArchitectApplication.java
@@ -0,0 +1,13 @@
+package fr.lne.SIArchitect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiArchitectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiArchitectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/lne/SIArchitect/SiArchitectApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lne.SIArchitect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiArchitectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
